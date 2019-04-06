2d5b0cd7d24b8508686246d7f2d113f64384084a
Bruno Paes <brunopaes05@gmail.com>
2019-04-06 14:07:15 -0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='TreeShrews',
+    version='0.1.0',
+    author=['Bruno Paes'],
+    author_email=['brunopaes05@gmail.com'],
+    packages=[''],
+    scripts=['src/propagation.py'],
+    url=None,
+    license=None,
+    description='ANN Module',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Pandas >= 0.24.2",
+    ])
