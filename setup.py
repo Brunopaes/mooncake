7e0352b04e910b71197652bcc7e4abe59c7f651f
Bruno Paes <brunopaes05@gmail.com>
2019-04-26 21:58:32 -0300

    Renaming Project

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='TreeShrews',
     version='0.1.0',
-    author=['Bruno Paes'],
-    author_email=['brunopaes05@gmail.com'],
-    packages=['treeshrews'],
-    scripts=['treeshrews/__init__.py'],
-    url='https://github.com/Brunopaes/TreeShrews',
+    author='Bruno Paes',
+    author_email='brunopaes05@gmail.com',
+    packages='miek',
+    scripts='miek/__init__.py',
+    url='https://github.com/Brunopaes/Miek',
     license='None',
     description='ANN Module',
     long_description=open('README.md').read())
